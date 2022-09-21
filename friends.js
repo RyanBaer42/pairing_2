7e06b0e0feb90ad727eb4ac5005681a72fde1533
Ryan Baer <ryanjbaer@outlook.com>
2022-09-21 15:23:01 -0600

    Add new friends

--- friends.js
+++ friends.js
@@ -1,7 +1,12 @@
-var bestFriend = "Max Pinch"
-var normalFriend = "Jake"
-if (bestFriend === "Max Pinch"){
-  console.log("Whats up " + bestFriend)
-} else {
-  console.log("Hey " + normalFriend)
+var friends = ["Max Pinch", "Brandon", "Jake"]
+
+
+for (let i = 0; i < friends.length; i++)
+    {if (friends[i] === "Max Pinch"){
+  console.log("Whats up " + friends[i])
+} else if (friends[i] === "Brandon"){
+  console.log("Yo its my friend " + friends[i])
 }
+else {
+  console.log("Hey " + friends[i])
+}}
